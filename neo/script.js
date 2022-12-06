b745da47b1c831f4b9252568b4088d2f461aa1fc
Niteletsplay <fragenwert@gmail.com>
2022-12-06 19:24:02 +0100

    Design

--- neo/script.js
+++ neo/script.js
@@ -1,97 +1,73 @@
-/*  clock */
-const hours = document.querySelector('.hours');
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
+import {jsonData} from "../data.js";
 
-/*  play button */
-const play = document.querySelector('.play');
-const pause = document.querySelector('.pause');
-const playBtn = document.querySelector('.circle__btn');
-const wave1 = document.querySelector('.circle__back-1');
-const wave2 = document.querySelector('.circle__back-2');
+console.log(jsonData)
 
-/*  rate slider */
-const container = document.querySelector('.slider__box');
-const btn = document.querySelector('.slider__btn');
-const color = document.querySelector('.slider__color');
-const tooltip = document.querySelector('.slider__tooltip');
+let data = JSON.parse(jsonData);
+var i;
 
-clock = () => {
-  let today = new Date();
-  let h = (today.getHours() % 12) + today.getMinutes() / 59; // 22 % 12 = 10pm
-  let m = today.getMinutes(); // 0 - 59
-  let s = today.getSeconds(); // 0 - 59
+const element = document.getElementById("searchbar");
 
-  h *= 30; // 12 * 30 = 360deg
-  m *= 6;
-  s *= 6; // 60 * 6 = 360deg
+element.addEventListener("keyup", search_users);
 
-  rotation(hours, h);
-  rotation(minutes, m);
-  rotation(seconds, s);
+search_users();
 
-  // call every second
-  setTimeout(clock, 500);
-}
 
-rotation = (target, val) => {
-  target.style.transform =  `rotate(${val}deg)`;
-}
+function search_users() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.querySelector('#list-holder');
+  x.innerHTML = ""
+  
 
-window.onload = clock();
+  for (const [i, _] of Object.entries(data)) {
+    let obj = data[i];
 
-dragElement = (target, btn) => {
-  target.addEventListener('mousedown', (e) => {
-      onMouseMove(e);
-      window.addEventListener('mousemove', onMouseMove);
-      window.addEventListener('mouseup', onMouseUp);
-  });
+    if (obj.name.toLowerCase().includes(input)) {
+      const base_div = document.createElement("div"); // Outer div
+      base_div.className = "chip";
 
-  onMouseMove = (e) => {
-      e.preventDefault();
-      let targetRect = target.getBoundingClientRect();
-      let x = e.pageX - targetRect.left + 10;
-      if (x > targetRect.width) { x = targetRect.width};
-      if (x < 0){ x = 0};
-      btn.x = x - 10;
-      btn.style.left = btn.x + 'px';
+      const p_icon_div = document.createElement("div"); // Icon div
+      p_icon_div.className = "chip__icon";
 
-      // get the position of the button inside the container (%)
-      let percentPosition = (btn.x + 10) / targetRect.width * 100;
+      const p_icon = document.createElement("ion-icon"); // Person icon
+      p_icon.className = "icon"
+      p_icon.setAttribute("name", "person");
+
+      const txt_div = document.createElement("div"); // Text container
+      txt_div.className = "text-container";
+
+      const name_p = document.createElement("p"); // Name paragraph
+
+      const v_icon_div = document.createElement("div"); // Icon div
+      v_icon_div.className = "chip__view";
+
+      const v_icon = document.createElement("ion-icon"); // View icon
+      v_icon.className = "icon"
+      v_icon.setAttribute("name", "eye-outline");
+
+      const e_icon_div = document.createElement("div"); // Icon div
+      e_icon_div.className = "chip__edit";
+
+      const e_icon = document.createElement("ion-icon"); // Edit icon
+      e_icon.className = "icon"
+      e_icon.setAttribute("name", "create-outline");
       
-      // color width = position of button (%)
-      color.style.width = percentPosition + "%";
 
-      // move the tooltip when button moves, and show the tooltip
-      tooltip.style.left = btn.x - 5 + 'px';
-      tooltip.style.opacity = 1;
-
-      // show the percentage in the tooltip
-      tooltip.textContent = Math.round(percentPosition) + '%';
-  };
-
-  onMouseUp  = (e) => {
-      window.removeEventListener('mousemove', onMouseMove);
-      tooltip.style.opacity = 0;
-
-      btn.addEventListener('mouseover', function() {
-        tooltip.style.opacity = 1;
-      });
       
-      btn.addEventListener('mouseout', function() {
-        tooltip.style.opacity = 0;
-      });
-  };
-};
+      base_div.appendChild(p_icon_div);
+      base_div.appendChild(txt_div);
+      txt_div.appendChild(name_p);
 
-dragElement(container, btn);
+      base_div.appendChild(v_icon_div);
+      base_div.appendChild(e_icon_div);
+      p_icon_div.appendChild(p_icon);
+      v_icon_div.appendChild(v_icon);
+      e_icon_div.appendChild(e_icon);
 
-/*  play button  */
-playBtn.addEventListener('click', function(e) {
-  e.preventDefault();
-  pause.classList.toggle('visibility');
-  play.classList.toggle('visibility');
-  playBtn.classList.toggle('shadow');
-  wave1.classList.toggle('paused');
-  wave2.classList.toggle('paused');
-});
+
+
+      name_p.innerHTML = `${obj.name}`
+      x.appendChild(base_div)
+    }
+  }
+}
