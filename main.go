1589cdbd238cf7eba8aa041f70ebf43a74724ccb
9glenda <plan9git@proton.me>
2022-12-08 18:44:35 +0000

    go fmt

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-  "net/http"
-  "embed"
-  "log"
-  api "github.com/niteletsplay/seekr/api"
+	"embed"
+	api "github.com/niteletsplay/seekr/api"
+	"log"
+	"net/http"
 )
 
 // web holds our web server content.
+//
 //go:embed web
 var content embed.FS
 var persons = make(api.DataBase)
 
 func main() {
-  go api.ServeApi(persons,":8080","data.json")
+	go api.ServeApi(persons, ":8080", "data.json")
 	// Serve files from static folder
-  http.Handle("/", http.FileServer(http.FS(content)))
+	http.Handle("/", http.FileServer(http.FS(content)))
 
-  println("web server running http://localhost:5050/web")
-  log.Fatal(http.ListenAndServe(":5050", nil))
+	println("web server running http://localhost:5050/web")
+	log.Fatal(http.ListenAndServe(":5050", nil))
 
 }
-
