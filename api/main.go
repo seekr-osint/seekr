67616cd99ca4d9a7ad9f18443fe4f0c7b0d0b62d
9glenda <plan9git@proton.me>
2022-12-08 18:44:18 +0000

    added data stuff

--- api/main.go
+++ api/main.go
@@ -16,9 +16,21 @@ type person struct {
 	ID      string `json:"id"`
 	Name    string `json:"name"`
 	Age     int8   `json:"age"`
-	Youtube string `json:"youtube"`
+  Birthday string `json:"bday"`
   Address string `json:"address"`
+  Phone string `json:"phone"`
   Civilstatus string `json:"civilstatus"`
+  Kids string `json:"kids"`
+  Hobbies string `json:"hobbies"`
+  Email string `json:"email"`
+  Occupation string `json:"occupation"`
+  Prevoccupation string `json:"prevoccupation"`
+  Military string `json:"military"`
+  Club string `json:"club"`
+  Legal string `json:"legal"`
+  Political string `json:"political"`
+  Notes string `json:"notes"`
+	Youtube string `json:"youtube"`
 }
 
 type DataBase map[string]person
