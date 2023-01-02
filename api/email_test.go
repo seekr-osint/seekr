da1edc36ea6b03079f7866c038bb17c133b3bcdc
9glenda <9glenda@users.noreply.github.com>
2023-01-02 03:03:40 +0000

    Commit from GitHub Actions (Go)
    Signed-off-by: 9glenda <9glenda@users.noreply.github.com>

--- api/email_test.go
+++ api/email_test.go
@@ -35,57 +35,57 @@ func TestIsEmailValid(t *testing.T) {
 }
 
 func TestIsGmailAddress(t *testing.T) {
-    // Test cases
-    testCases := []struct {
-        email     string
-        expect    bool
-    }{
-        {"user@gmail.com", true},
-        {"user.name@gmail.com", true},
-        {"user_name@gmail.com", true},
-        {"user-name@gmail.com", true},
-        {"user@googlemail.com", false},
-        {"user@example.com", false},
-        {"user@gmail.co.uk", false},
-        {"user@gmail.", false},
-        {"@gmail.com", false},
-    }
+	// Test cases
+	testCases := []struct {
+		email  string
+		expect bool
+	}{
+		{"user@gmail.com", true},
+		{"user.name@gmail.com", true},
+		{"user_name@gmail.com", true},
+		{"user-name@gmail.com", true},
+		{"user@googlemail.com", false},
+		{"user@example.com", false},
+		{"user@gmail.co.uk", false},
+		{"user@gmail.", false},
+		{"@gmail.com", false},
+	}
 
-    // Loop through test cases
-    for _, tc := range testCases {
-        result := IsGmailAddress(tc.email)
-        if result != tc.expect {
-            t.Errorf("Expected %t for %s, got %t", tc.expect, tc.email, result)
-        }
-    }
+	// Loop through test cases
+	for _, tc := range testCases {
+		result := IsGmailAddress(tc.email)
+		if result != tc.expect {
+			t.Errorf("Expected %t for %s, got %t", tc.expect, tc.email, result)
+		}
+	}
 }
 func TestIsValidGmailAddress(t *testing.T) {
-    // Test cases
-    testCases := []struct {
-        email     string
-        expect    bool
-    }{
-        {"user@gmail.com", true},
-        {"user.name@gmail.com", true},
-        {"user.name1@gmail.com", true},
-        {"user.1name@gmail.com", true},
-        {"user-name@gmail.com", false},
-        {"user_name@gmail.com", false},
-        {"user.name@googlemail.com", false},
-        {"user.name@example.com", false},
-        {"user.name@gmail.co.uk", false},
-        {"user.name@gmail.", false},
-        {"@gmail.com", false},
-        {"user.name@gmail.c", false},
-        {"user.name@gmail.co", false},
-        {"user.name@gmail.c1", false},
-        {"user.name@gmail.1com", false},
-    }
-        // Loop through test cases
-    for _, tc := range testCases {
-        result := IsValidGmailAddress(tc.email)
-        if result != tc.expect {
-            t.Errorf("Expected %t for %s, got %t", tc.expect, tc.email, result)
-        }
-    }
+	// Test cases
+	testCases := []struct {
+		email  string
+		expect bool
+	}{
+		{"user@gmail.com", true},
+		{"user.name@gmail.com", true},
+		{"user.name1@gmail.com", true},
+		{"user.1name@gmail.com", true},
+		{"user-name@gmail.com", false},
+		{"user_name@gmail.com", false},
+		{"user.name@googlemail.com", false},
+		{"user.name@example.com", false},
+		{"user.name@gmail.co.uk", false},
+		{"user.name@gmail.", false},
+		{"@gmail.com", false},
+		{"user.name@gmail.c", false},
+		{"user.name@gmail.co", false},
+		{"user.name@gmail.c1", false},
+		{"user.name@gmail.1com", false},
+	}
+	// Loop through test cases
+	for _, tc := range testCases {
+		result := IsValidGmailAddress(tc.email)
+		if result != tc.expect {
+			t.Errorf("Expected %t for %s, got %t", tc.expect, tc.email, result)
+		}
+	}
 }
