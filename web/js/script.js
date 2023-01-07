b58ed0caf072a41740a23a91b8e3c0eba7fd0e10
Nite <fragenwert@gmail.com>
2023-01-07 11:16:31 +0100

    Paste format fix

--- web/js/script.js
+++ web/js/script.js
@@ -554,3 +554,67 @@ async function main() {
 }
 
 main()
+
+document.querySelectorAll("span").forEach(function (element) {
+  element.addEventListener('paste', function (e) {
+    // Prevent the default action
+    e.preventDefault();
+
+    // Get the copied text from the clipboard
+    const text = e.clipboardData
+      ? (e.originalEvent || e).clipboardData.getData('text/plain')
+      : // For IE
+      window.clipboardData
+      ? window.clipboardData.getData('Text')
+      : '';
+
+    if (document.queryCommandSupported('insertText')) {
+      document.execCommand('insertText', false, text);
+    } else {
+      // Insert text at the current position of caret
+      const range = document.getSelection().getRangeAt(0);
+      range.deleteContents();
+
+      const textNode = document.createTextNode(text);
+      range.insertNode(textNode);
+      range.selectNodeContents(textNode);
+      range.collapse(false);
+
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  });
+});
+
+[document.getElementById("c-name-tag"), document.getElementById("acc-name-tag")].forEach(item => {
+  item.addEventListener('paste', function (e) {
+    // Prevent the default action
+    e.preventDefault();
+  
+    // Get the copied text from the clipboard
+    const text = e.clipboardData
+        ? (e.originalEvent || e).clipboardData.getData('text/plain')
+        : // For IE
+        window.clipboardData
+        ? window.clipboardData.getData('Text')
+        : '';
+  
+    if (document.queryCommandSupported('insertText')) {
+        document.execCommand('insertText', false, text);
+    } else {
+        // Insert text at the current position of caret
+        const range = document.getSelection().getRangeAt(0);
+        range.deleteContents();
+  
+        const textNode = document.createTextNode(text);
+        range.insertNode(textNode);
+        range.selectNodeContents(textNode);
+        range.collapse(false);
+  
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+  });
+});
