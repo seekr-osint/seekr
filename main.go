439653104bbbbe6a1021b857f01f247218fd4eec
9glenda <plan9git@proton.me>
2023-01-14 20:29:08 +0000

    flex how MANY services we support when starting seekr

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"embed"
 	"fmt"
+	"strconv"
 
 	api "github.com/seekr-osint/seekr/api"
 	webServer "github.com/seekr-osint/seekr/webServer"
@@ -17,7 +18,7 @@ var content embed.FS
 var people = make(api.DataBase)
 
 func main() {
-
+  fmt.Println("Welcome to seekr a powerful OSINT tool able to scan the web for " + strconv.Itoa(len(api.DefaultServices)) + "")
 	go api.ServeApi(people, ":8080", "data.json") // TODO config parsing stuff
 	RunWebServer()
 	fmt.Println(api.ServicesHandler(api.DefaultServices, "9glenda"))
