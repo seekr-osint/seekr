8b09d9c0a623b66b9a78e2c01db7672d6895db72
9glenda <plan9git@proton.me>
2023-01-18 20:29:47 +0000

    made not work stuff work stuff (tests)

---
+++ api/services_test.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"testing"
+)
+
+func TestUsernames(t *testing.T) {
+	username := RandomString(16)
+	for i := 0; i < len(DefaultServices); i++ { // loop over all services
+		service := DefaultServices[i]                  // current service
+		if service.UserExistsFunc(service, username) { // if service exisits
+			t.Errorf("This no work %s", service.Name)
+		}
+	}
+}
---
+++ api/stuff.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomChar() string {
+	rand.Seed(time.Now().UnixNano())
+	charset := "abcdefghijklmnopqrstuvwxyz1234567890"
+	return string(charset[rand.Intn(len(charset))])
+}
+func RandomString(cnt int) string {
+	str := ""
+	for i := 0; i < cnt; i++ {
+		str = str + RandomChar()
+	}
+	return str
+}
