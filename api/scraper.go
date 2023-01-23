7ee2a8302b1ed6f58ebcd97a7f75125accf55089
9glenda <plan9git@proton.me>
2023-01-23 13:52:29 +0000

    stuffffffff

---
+++ api/scraper.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+  //"github.com/gocolly/colly"
+  //"log"
+)
+
+func Scrape(url string) {
+
+  	//c := colly.NewCollector(
+		// Visit only domains: old.reddit.com
+		//colly.AllowedDomains("old.reddit.com"),
+		// Parallelism
+		//colly.Async(true),
+	//)
+  //log.Println("hello")
+}
