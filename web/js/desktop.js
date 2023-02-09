8c3084eeda78a81722349425d15d522bb4b9dcf9
Nite <67828948+Niteletsplay@users.noreply.github.com>
2023-02-09 18:22:08 +0100

    stuff

--- web/js/desktop.js
+++ web/js/desktop.js
@@ -1,18 +1,31 @@
-const frame = document.querySelector('.frame');
-
 function createSeekrWindow() {
-new WinBox("SEEKR", {
-  id: "seekr-window",
-  html: '<iframe class="frame" src="./index.html"></iframe>',
-  background: "#E4EBF5",
-  header: 45,
-  // viewport boundaries:
-  top: 10,
-  right: 10,
-  bottom: 0,
-  left: 10,
-});
+  new WinBox("SEEKR", {
+    id: "seekr-window",
+    html: '<iframe class="frame" src="./index.html"></iframe>',
+    background: "#E4EBF5",
+    header: 45,
+    // viewport boundaries:
+    top: 10,
+    right: 10,
+    bottom: 0,
+    left: 10,
+  });
 }
+
+function createWhoisWindow() {
+  new WinBox("WHOIS", {
+    id: "seekr-window",
+    html: '<iframe class="frame" src="https://who.is"></iframe>',
+    background: "#E4EBF5",
+    header: 45,
+    // viewport boundaries:
+    top: 10,
+    right: 10,
+    bottom: 0,
+    left: 10,
+  });
+}
+
 function toggleAppBar() {
   var appBar = document.querySelector(".app-bar");
   if (appBar.style.display === "none") {
