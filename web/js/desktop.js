29b42f748e577a7d9b7dae7ed9bdc739d0b361ca
Nite <67828948+Niteletsplay@users.noreply.github.com>
2023-02-09 15:55:40 +0100

    not working
    Co-authored-by: 9glenda <plan9git@proton.me>

--- web/js/desktop.js
+++ web/js/desktop.js
@@ -1,17 +1,5 @@
 const frame = document.querySelector('.frame');
 
-new WinBox("CUSTOM", {
-  id: "seekr-window",
-  html: '<iframe class="frame" src="https://www.google.com/webhp?igu=1"></iframe>',
-  background: "#E4EBF5",
-  header: 45,
-  // viewport boundaries:
-  top: 10,
-  right: 10,
-  bottom: 0,
-  left: 10,
-});
-
 new WinBox("SEEKR", {
   id: "seekr-window",
   html: '<iframe class="frame" src="./index.html"></iframe>',
