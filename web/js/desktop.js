c34cbccbfa914d96dad8344b2feed5e8eab3cee0
9glenda <plan9git@proton.me>
2023-02-09 16:46:47 +0000

    added settings to desktop

--- web/js/desktop.js
+++ web/js/desktop.js
@@ -13,6 +13,21 @@ new WinBox("SEEKR", {
   left: 10,
 });
 }
+
+function createSeekrSettingsWindow() {
+new WinBox("SEEKR SETTINGS", {
+  id: "seekr-window",
+  html: '<iframe class="frame" src="./settings.html"></iframe>',
+  background: "#E4EBF5",
+  header: 45,
+  // viewport boundaries:
+  top: 10,
+  right: 10,
+  bottom: 0,
+  left: 10,
+});
+}
+
 function toggleAppBar() {
   var appBar = document.querySelector(".app-bar");
   if (appBar.style.display === "none") {
