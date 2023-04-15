58b334ade6aa71096eae412a4e66a83d351a82e6
9glenda <plan9git@proton.me>
2023-04-15 21:34:24 +0000

    update vendor hash

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
             src = ./.;
 
             #vendorSha256 = pkgs.lib.fakeSha256;
-            vendorSha256 = "sha256-5Bae+SRQbDOUP8aTXS+tGd5Ga+phM0cvCDmmpg8KdNU=";
+            vendorSha256 = "sha256-Alq3l3/bpDCqK8d0wkgqoa0Jyi+J9linXgCiQD6QRt4=";
           };
         });
 
