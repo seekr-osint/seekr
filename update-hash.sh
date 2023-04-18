6bd7a5d25b6263f0ef00770e6e89dfdf742ff3ca
9glenda <plan9git@proton.me>
2023-04-18 13:47:26 +0000

    go tidy support

--- update-hash.sh
+++ update-hash.sh
@@ -4,12 +4,16 @@
 FLAKE="./flake.nix"
 old_line_with_hash="$(grep 'vendorSha256 = "sha256' "$FLAKE")"
 old_hash="$(printf '%s' "$old_line_with_hash" | cut -d'"' -f2)"
+tidy="$(go mod tidy -v)"
 new() {
   rm -rf ./vendor
   go mod vendor
   nix hash path ./vendor/
   rm -rf ./vendor
 }
+if [ "$tidy" != "\n" ]; then
+  printf 'go.mod\ngo.sum\n'
+fi
 new_hash="$(new)"
 if [ "$old_hash" != "$new_hash" ];then
   sed -i "s|$old_hash|$new_hash|g" flake.nix
