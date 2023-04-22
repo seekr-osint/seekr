719ff739a70a318b4fa46c1d1116b04fb853a90f
9glenda <plan9git@proton.me>
2023-04-22 19:30:36 +0000

    ts init

--- flake.nix
+++ flake.nix
@@ -69,6 +69,9 @@
             nixpkgsFor.${system}.jq
             nixpkgsFor.${system}.goreleaser
             self.packages.${system}.seekr
+
+
+            nixpkgsFor.${system}.typescript
           ];
         };
       });
