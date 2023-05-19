e533ae680b226389aea8bae22e461bbab05a0d86
Nite <67828948+Niteletsplay@users.noreply.github.com>
2023-05-19 19:02:37 +0200

    Added manipulation of icon colors

--- web/ts/theme_loader.ts
+++ web/ts/theme_loader.ts
@@ -38,43 +38,7 @@ fetch(cssFolder)
 
       const theme = href.trim().replace(/^\.\/themes\/|\.css$/g, '');
 
-      if (theme != "default") {
-        createThemeCards(theme);
-      }
+      createThemeCards(theme);
     });
   }
 );
-
-function recolorImage(img: any, oldRed: Number, oldGreen: Number, oldBlue: Number, newRed: Number, newGreen: Number, newBlue: Number) {
-  var c = document.createElement('canvas');
-  var ctx = c.getContext("2d");
-  var w = img.width;
-  var h = img.height;
-
-  c.width = w;
-  c.height = h;
-
-  // draw the image on the temporary canvas
-  ctx.drawImage(img, 0, 0, w, h);
-
-  // pull the entire image into an array of pixel data
-  var imageData = ctx.getImageData(0, 0, w, h);
-
-  // examine every pixel, 
-  // change any old rgb to the new-rgb
-  for (var i = 0; i < imageData.data.length; i += 4) {
-      // is this pixel the old rgb?
-      if (imageData.data[i] == oldRed && imageData.data[i + 1] == oldGreen && imageData.data[i + 2] == oldBlue) {
-          // change to your new rgb
-          imageData.data[i] = newRed;
-          imageData.data[i + 1] = newGreen;
-          imageData.data[i + 2] = newBlue;
-      }
-  }
-  // put the altered data back on the canvas  
-  ctx.putImageData(imageData, 0, 0);
-  // put the re-colored image back on the image
-  var img1 = document.getElementById("image1");
-  img1.src = c.toDataURL('image/png');
-
-}
