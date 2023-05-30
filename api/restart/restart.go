eee342ed873c2d81e871d91361769ff8ae0cae5e
Nite <67828948+Niteletsplay@users.noreply.github.com>
2023-05-30 12:17:21 +0200

    Works on windows (for real this time)

--- api/restart/restart.go
+++ api/restart/restart.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"os"
 	"os/exec"
+	"runtime"
+	"syscall"
 )
 
 var (
@@ -18,21 +20,30 @@ func RestartBinary() error {
 		return ErrGetCurrentBinary
 	}
 
-	// Start a new process with the current executable
-	cmd := exec.Command(executable, os.Args[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		if err := syscall.Exec(executable, os.Args, os.Environ()); err != nil {
+			return ErrStartNewBin
+		}
+	} else if runtime.GOOS == "windows" {
+		// Start a new instance of the current binary
+		cmd := exec.Command(executable, os.Args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+		cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP}
+		if err := cmd.Start(); err != nil {
+			return ErrStartNewBin
+		}
 
-	err = cmd.Start()
-	if err != nil {
-		return ErrStartNewBin
-	}
-
-	// Wait for the new process to finish
-	err = cmd.Wait()
-	if err != nil {
-		return err
+		// Terminate the current process
+		currentProcess, err := syscall.GetCurrentProcess()
+		if err != nil {
+			return err
+		}
+		err = syscall.TerminateProcess(currentProcess, 0)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
