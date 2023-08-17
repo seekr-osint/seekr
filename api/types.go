c485f6edb823fc591f47247a9df9b344fde701bd
9glenda <plan9git@proton.me>
2023-08-18 00:56:26 +0200

    added new accounts as invisible json field

--- api/types.go
+++ api/types.go
@@ -8,6 +8,7 @@ import (
 	"github.com/seekr-osint/seekr/api/hobby"
 	"github.com/seekr-osint/seekr/api/ip"
 	"github.com/seekr-osint/seekr/api/religion"
+	"github.com/seekr-osint/seekr/api/services"
 	"github.com/seekr-osint/seekr/api/sources"
 )
 
@@ -15,37 +16,38 @@ import (
 
 // main data set
 type Person struct {
-	ID             string                  `json:"id" ts_transform:"__VALUE__ || ''"`
-	Name           string                  `json:"name" ts_transform:"__VALUE__ || ''"`
-	Gender         gender.Gender           `json:"gender" ts_transform:"__VALUE__ || ''"`
-	Ethnicity      ethnicity.Ethnicity     `json:"ethnicity" ts_transform:"__VALUE__ || ''"`
-	Pictures       Pictures                `json:"pictures"`
-	Maidenname     string                  `json:"maidenname" ts_transform:"__VALUE__ || ''"`
-	Age            Age                     `json:"age" ts_transform:"__VALUE__ || 0` // has to be a float64 becuase of json Unmarshal
-	Birthday       string                  `json:"bday" ts_transform:"__VALUE__ || ''"`
-	Address        string                  `json:"address" ts_transform:"__VALUE__ || ''"`
-	Phone          PhoneNumbers            `json:"phone"`
-	Ips            ip.Ips                  `json:"ips"`
-	Civilstatus    civilstatus.CivilStatus `json:"civilstatus" ts_transform:"__VALUE__ || ''"`
-	Kids           string                  `json:"kids" ts_transform:"__VALUE__ || ''"`
-	Hobbies        hobby.Hobbies           `json:"hobbies"`
-	Email          EmailsType              `json:"email"`
-	Occupation     string                  `json:"occupation" ts_transform:"__VALUE__ || ''"`
-	Prevoccupation string                  `json:"prevoccupation" ts_transform:"__VALUE__ || ''"`
-	Education      string                  `json:"education" ts_transform:"__VALUE__ || ''"`
-	Military       string                  `json:"military" ts_transform:"__VALUE__ || ''"`
-	Religion       religion.Religion       `json:"religion" ts_transform:"__VALUE__ || ''"`
-	Pets           string                  `json:"pets" ts_transform:"__VALUE__ || ''"`
-	Clubs          club.Clubs              `json:"clubs"`
-	Legal          string                  `json:"legal" ts_transform:"__VALUE__ || ''"`
-	Political      string                  `json:"political" ts_transform:"__VALUE__ || ''"`
-	Notes          string                  `json:"notes" ts_transform:"__VALUE__ || ''"`
-	Relations      Relation                `json:"relations"` // FIXME
-	Sources        sources.Sources         `json:"sources"`
-	Accounts       Accounts                `json:"accounts"`
-	Tags           Tags                    `json:"tags"`
-	NotAccounts    map[string]Account      `json:"notaccounts"`
-	Custom         interface{}             `json:"custom"`
+	ID             string                         `json:"id" ts_transform:"__VALUE__ || ''"`
+	Name           string                         `json:"name" ts_transform:"__VALUE__ || ''"`
+	Gender         gender.Gender                  `json:"gender" ts_transform:"__VALUE__ || ''"`
+	Ethnicity      ethnicity.Ethnicity            `json:"ethnicity" ts_transform:"__VALUE__ || ''"`
+	Pictures       Pictures                       `json:"pictures"`
+	Maidenname     string                         `json:"maidenname" ts_transform:"__VALUE__ || ''"`
+	Age            Age                            `json:"age" ts_transform:"__VALUE__ || 0` // has to be a float64 becuase of json Unmarshal
+	Birthday       string                         `json:"bday" ts_transform:"__VALUE__ || ''"`
+	Address        string                         `json:"address" ts_transform:"__VALUE__ || ''"`
+	Phone          PhoneNumbers                   `json:"phone"`
+	Ips            ip.Ips                         `json:"ips"`
+	Civilstatus    civilstatus.CivilStatus        `json:"civilstatus" ts_transform:"__VALUE__ || ''"`
+	Kids           string                         `json:"kids" ts_transform:"__VALUE__ || ''"`
+	Hobbies        hobby.Hobbies                  `json:"hobbies"`
+	Email          EmailsType                     `json:"email"`
+	Occupation     string                         `json:"occupation" ts_transform:"__VALUE__ || ''"`
+	Prevoccupation string                         `json:"prevoccupation" ts_transform:"__VALUE__ || ''"`
+	Education      string                         `json:"education" ts_transform:"__VALUE__ || ''"`
+	Military       string                         `json:"military" ts_transform:"__VALUE__ || ''"`
+	Religion       religion.Religion              `json:"religion" ts_transform:"__VALUE__ || ''"`
+	Pets           string                         `json:"pets" ts_transform:"__VALUE__ || ''"`
+	Clubs          club.Clubs                     `json:"clubs"`
+	Legal          string                         `json:"legal" ts_transform:"__VALUE__ || ''"`
+	Political      string                         `json:"political" ts_transform:"__VALUE__ || ''"`
+	Notes          string                         `json:"notes" ts_transform:"__VALUE__ || ''"`
+	Relations      Relation                       `json:"relations"` // FIXME
+	Sources        sources.Sources                `json:"sources"`
+	Accounts       Accounts                       `json:"accounts"`
+	NewAccounts    services.MapServiceCheckResult `json:"-"`
+	Tags           Tags                           `json:"tags"`
+	NotAccounts    map[string]Account             `json:"notaccounts"`
+	Custom         interface{}                    `json:"custom"`
 }
 
 type Relation map[string][]string
