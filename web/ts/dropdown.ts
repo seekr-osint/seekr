efb5ec0e203cd413b66bd213e6c4e6d6af40825f
9glenda <plan9git@proton.me>
2023-08-21 19:23:06 +0200

    added event listener "change"

--- web/ts/dropdown.ts
+++ web/ts/dropdown.ts
@@ -1,30 +1,34 @@
 const dropdowns: Set<string> = new Set([]);
 
 
-class CustomOption extends HTMLElement {
+class CustomDropdown extends HTMLElement {
   constructor() {
     super();
-    const id = this.textContent || ""
+    const id = this.getAttribute("dropdown-id") || ""
     if (id == "") {
-      console.log("error no dropdown name")
+      console.error("error no dropdown name")
+      return
+    } else if (dropdowns.has(id)) {
+      console.error("ID" + id + "already exsists");
       return
     }
-    this.setAttribute('id', id);
     dropdowns.add(id);
 
-    const options = this.getAttribute('options') || "";
+    const options = this.getAttribute("options") || "";
     if (options == "") {
-      console.log("error empty dropdown options")
+      console.error("error empty dropdown options")
+      dropdowns.delete(id)
       return
     }
 
-    const placeholder = this.getAttribute('placeholder') || "Select Item";
-    this.attachShadow({ mode: 'open' });
+    const placeholder = this.getAttribute("placeholder") || "Select Item";
+    this.attachShadow({ mode: "open" });
 
     this.shadowRoot!.innerHTML = `
       <link rel="stylesheet" href="./css/style.css">
       <link rel="stylesheet" href="./css/dropdown.css">
 
+      ${id}:
       <div class="dropdown">
         <input class="text-box" type="text" placeholder="${placeholder}" readonly>
         <div class="options">
@@ -40,26 +44,27 @@ class CustomOption extends HTMLElement {
       };
     }
 
-    const internalDivs = this.shadowRoot!.querySelectorAll('.dropdown-option');
+    const internalDivs = this.shadowRoot!.querySelectorAll(".dropdown-option");
     internalDivs.forEach((internalDiv) => {
-      internalDiv.addEventListener('click', () => {
-        const optionName = internalDiv.textContent || '';
+      internalDiv.addEventListener("click", () => {
+        const optionName = internalDiv.textContent || "";
         const textBox = this.shadowRoot!.querySelector(".text-box") as HTMLInputElement;
         if (textBox) {
           textBox.value = optionName;                  
         }
-        this.setAttribute('value', optionName);
+        this.setAttribute("value", optionName);
+        this.dispatchEvent(new Event("change"));
       });
     });
   }
 }
 
-customElements.define('custom-dropdown', CustomOption);
+customElements.define("custom-dropdown", CustomDropdown);
 
 function divTemplate(words: string): string {
-  const wordArray = words.split(',').map(word => word.trim());
+  const wordArray = words.split(",").map(word => word.trim());
 
-  let output = '';
+  let output = "";
 
   wordArray.forEach(word => {
     output += `<div class="dropdown-option">${word}</div>`;
@@ -68,21 +73,35 @@ function divTemplate(words: string): string {
 
   return output;
 }
+const button = document.querySelector("#myButton");
+ 
+button!.addEventListener("click", function() {
+   console.log(getValue("test1"));
+ });
 
-const button = document.getElementById('myButton');
+const d = getDropdown("test1")
 
-button!.addEventListener('click', function() {
+d!.addEventListener("change", function() {
   console.log(getValue("test1"));
 });
 
+function getDropdown(id: string): Element | null {
+  if (!dropdowns.has(id)) {
+    console.error("Error: no dropdown with this name: " + id)
+    return null
+  }
+  return document.querySelector(`custom-dropdown[dropdown-id="${id}"]`);
+}
+
 function getValue(id: string): string {
   if (!dropdowns.has(id)) {
-    console.log("Error: no dropdown with this name")
+    console.error("Error: no dropdown with this name: " + id)
+    return ""
   }
-  const customOptionElement = document.querySelector(`custom-dropdown#${id}`);
+  const customOptionElement = getDropdown(id);
   
   return customOptionElement!.getAttribute("value") || ""
 }
 
 
-export { getValue };
+export { getValue, getDropdown };
