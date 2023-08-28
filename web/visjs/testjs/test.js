e25e068de928b700a2bb722fffac81ef6a6ff5b5
Nite <fragenwert@gmail.com>
2023-08-28 23:05:06 +0200

    i am going insane

--- web/visjs/testjs/test.js
+++ web/visjs/testjs/test.js
@@ -120,10 +120,11 @@ var edges = [
 
 // create a network
 
-var data = {
-    nodes: nodes,
-    edges: edges,
-};
+var nodesDataset = new vis.DataSet(nodes); // these come from WorldCup2014.js
+var edgesDataset = new vis.DataSet(edges); // these come from WorldCup2014.js
+
+var data = { nodes: nodesDataset, edges: edgesDataset };
+
 var options = {
     layout: { randomSeed: 8 },
     physics: { adaptiveTimestep: false },
@@ -203,3 +204,99 @@ function openClusters(scale) {
 
     network.stabilize();
 }
+
+
+
+
+
+
+var allNodes;
+var highlightActive = false;
+
+
+
+function redrawAll() {
+    // get a JSON object
+    allNodes = nodesDataset.get({ returnType: "Object" });
+
+    network.on("click", neighbourhoodHighlight);
+}
+
+function neighbourhoodHighlight(params) {
+    // if something is selected:
+    if (params.nodes.length > 0) {
+        highlightActive = true;
+        var i, j;
+        var selectedNode = params.nodes[0];
+        var degrees = 2;
+
+        // mark all nodes as hard to read.
+        for (var nodeId in allNodes) {
+            allNodes[nodeId].color = "rgba(200,200,200,0.5)";
+            if (allNodes[nodeId].hiddenLabel === undefined) {
+                allNodes[nodeId].hiddenLabel = allNodes[nodeId].label;
+                allNodes[nodeId].label = undefined;
+            }
+        }
+        var connectedNodes = network.getConnectedNodes(selectedNode);
+        var allConnectedNodes = [];
+
+        // get the second degree nodes
+        for (i = 1; i < degrees; i++) {
+            for (j = 0; j < connectedNodes.length; j++) {
+                allConnectedNodes = allConnectedNodes.concat(
+                    network.getConnectedNodes(connectedNodes[j])
+                );
+            }
+        }
+
+        // all second degree nodes get a different color and their label back
+        for (i = 0; i < allConnectedNodes.length; i++) {
+            allNodes[allConnectedNodes[i]].color = "rgba(150,150,150,0.75)";
+            if (allNodes[allConnectedNodes[i]].hiddenLabel !== undefined) {
+                allNodes[allConnectedNodes[i]].label =
+                    allNodes[allConnectedNodes[i]].hiddenLabel;
+                allNodes[allConnectedNodes[i]].hiddenLabel = undefined;
+            }
+        }
+
+        // all first degree nodes get their own color and their label back
+        for (i = 0; i < connectedNodes.length; i++) {
+            allNodes[connectedNodes[i]].color = undefined;
+            if (allNodes[connectedNodes[i]].hiddenLabel !== undefined) {
+                allNodes[connectedNodes[i]].label =
+                    allNodes[connectedNodes[i]].hiddenLabel;
+                allNodes[connectedNodes[i]].hiddenLabel = undefined;
+            }
+        }
+
+        // the main node gets its own color and its label back.
+        allNodes[selectedNode].color = undefined;
+        if (allNodes[selectedNode].hiddenLabel !== undefined) {
+            allNodes[selectedNode].label = allNodes[selectedNode].hiddenLabel;
+            allNodes[selectedNode].hiddenLabel = undefined;
+        }
+    } else if (highlightActive === true) {
+        // reset all nodes
+
+        for (var nodeId in allNodes) {
+            allNodes[nodeId].color = undefined;
+            if (allNodes[nodeId].hiddenLabel !== undefined) {
+                allNodes[nodeId].label = allNodes[nodeId].hiddenLabel;
+                allNodes[nodeId].hiddenLabel = undefined;
+            }
+        }
+        highlightActive = false;
+    }
+
+    // transform the object into an array
+    var updateArray = [];
+    for (nodeId in allNodes) {
+        if (allNodes.hasOwnProperty(nodeId)) {
+            updateArray.push(allNodes[nodeId]);
+        }
+    }
+    nodesDataset.update(updateArray);
+}
+
+redrawAll();
